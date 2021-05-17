8f6c1acb81da23b1551116ba86ecb5bf9c645c53
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-17 19:16:31 +0200

    create an HttpRequest class working with http package

---
+++ lib/services/HttpRequest.dart
@@ -0,0 +1,19 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class HttpRequest {
+
+  static dynamic httpRequestGet(Uri url) async {
+    http.Response response = await http.get(url);
+    var data = response.body;
+
+    if(response.statusCode == 200 && data != null) {
+      print('status code : ${response.statusCode} -> data retrieved : $data');
+      return jsonDecode(data);
+    } else {
+      throw Exception('failed to retrieve data : response code : ${response.statusCode}');
+    }
+  }
+}
