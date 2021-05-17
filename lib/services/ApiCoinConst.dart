4b40727d70b53e3f3035175a19b22d4f63bcdaef
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-17 19:15:48 +0200

    add Api Coin consts

---
+++ lib/services/ApiCoinConst.dart
@@ -0,0 +1,7 @@
+import 'package:bitcoin_ticker/services/apiKey.dart';
+
+class ApiCoinConst {
+  static const String authority = 'min-api.cryptocompare.com';
+  static const String unencodedPath = '/data/pricemulti';
+  static const String apiKey = privateKey; //value come from a file not followed by git
+}
