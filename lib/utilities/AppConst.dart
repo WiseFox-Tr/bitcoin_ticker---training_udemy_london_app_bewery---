0a4d38f601f4cc8b6fde882e06cd87d31bec8bd9
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-17 12:40:21 +0200

    add AppConst class

---
+++ lib/utilities/AppConst.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppConst {
+  static const TextStyle pickerTextStyle = TextStyle(color: Colors.white);
+  static const TextStyle mainTextStyle = TextStyle(color: Colors.white, fontSize: 20.0);
+}
