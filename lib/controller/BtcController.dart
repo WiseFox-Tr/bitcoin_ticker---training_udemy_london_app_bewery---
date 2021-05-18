239dae18477346522656ac7169616fe0351d22e3
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-18 19:43:05 +0200

    get callback methods for AppSelector & add a try{}catch{} bloc when app tries to get web data

--- lib/controller/BtcController.dart
+++ lib/controller/BtcController.dart
@@ -1,5 +1,6 @@
 import 'package:bitcoin_ticker/model/CryptoRatio.dart';
 import 'package:bitcoin_ticker/services/WebServices.dart';
+import 'package:bitcoin_ticker/utilities/AppConst.dart';
 import 'package:bitcoin_ticker/utilities/coin_data.dart' as currencies;
 import 'package:bitcoin_ticker/view/widgets/CryptoRatioCard.dart';
 import 'package:flutter/material.dart';
@@ -15,14 +16,18 @@ class BtcController {
   set setCurrentFiat(String newFiat) => _currentFiat = newFiat;
 
   Future<void> getCryptoRate() async {
-    _cryptoRatioList = await WebServices.getCryptoRate(currencies.cryptoCurrencyNames, currencies.fiatCurrencyNames);
-    _cryptoRatioList.forEach((crypto) => print(crypto.toString())); //print verification
+    try {
+      _cryptoRatioList = await WebServices.getCryptoRate(currencies.cryptoCurrencyNames, currencies.fiatCurrencyNames);
+      _cryptoRatioList.forEach((crypto) => print(crypto.toString())); //print verification
+    } catch(e) {
+      print('exception : $e');
+    }
   }
 
   ///for each crypto, it save current rate when for this crypto, fiat name & current fiat are same & add a CryptoCardWidget to widget List !
   ///
   ///At the end, it returns the list widget with one CryptoRatioWidget by crypto
-  List<Widget> getCurrencyCards () {
+  List<Widget> updateCurrencyCards () {
     String _currentRate = '?';
     _currencyCards.clear();
     currencies.cryptoCurrencyNames.forEach((cryptoName) {
@@ -34,5 +39,19 @@ class BtcController {
       _currencyCards.add(CryptoRatioCard(cryptoName: cryptoName, rate: _currentRate, fiatName: _currentFiat));
     });
     return _currencyCards;
+  }
+
+  List<Text> getIosCurrencyItems() {
+    List<Text> fiatCurrencies = [];
+    currencies.fiatCurrencyNames.forEach((nationalCurrency) {
+      fiatCurrencies.add(Text(nationalCurrency, style: AppConst.pickerTextStyle));
+    });
+    return fiatCurrencies;
+  }
+
+  List<DropdownMenuItem<String>> getAndroidCurrencyItems() {
+    return currencies.fiatCurrencyNames.map<DropdownMenuItem<String>>((String value) {
+      return DropdownMenuItem(value: value, child: Text(value));
+    }).toList();
   }
 }
