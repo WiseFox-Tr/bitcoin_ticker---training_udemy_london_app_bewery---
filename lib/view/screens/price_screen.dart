5bbcefc659919db193737e42957d55e16a837c93
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-18 18:33:24 +0200

    delete the default cryptoRatioCard widget & call getCurrencyCards from controller. Also add set state for ios app selector callback

--- lib/view/screens/price_screen.dart
+++ lib/view/screens/price_screen.dart
@@ -22,24 +22,7 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: AppConst.mainTextStyle,
-                ),
-              ),
-            ),
-          ),
+          Column(children: _btcController.getCurrencyCards()),
           Container(
             height: 150.0,
             alignment: Alignment.center,
@@ -59,7 +42,7 @@ class _PriceScreenState extends State<PriceScreen> {
                   iosPickerItems: getIosCurrencyItems(),
                   onIosSelectedItemChanged: (index) {
                     print(index);
-                    _btcController.setCurrentFiat = _btcController.getFiatCurrencies[index];
+                    setState(() => _btcController.setCurrentFiat = _btcController.getFiatCurrencies[index]);
                     print('Current fiat currency : ${_btcController.getCurrentFiat}');
                   },
                 ),
