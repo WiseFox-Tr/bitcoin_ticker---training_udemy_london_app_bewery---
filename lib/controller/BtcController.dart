a0fe87ffbe6f4b54436fdeb7209361801efef294
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-18 18:30:45 +0200

    create a method that returns a list of CryptoRatioWidget (one for each crypto name) with values

--- lib/controller/BtcController.dart
+++ lib/controller/BtcController.dart
@@ -1,22 +1,38 @@
-import 'package:bitcoin_ticker/model/Crypto.dart';
+import 'package:bitcoin_ticker/model/CryptoRatio.dart';
 import 'package:bitcoin_ticker/services/WebServices.dart';
 import 'package:bitcoin_ticker/utilities/coin_data.dart' as currencies;
+import 'package:bitcoin_ticker/view/widgets/CryptoRatioCard.dart';
+import 'package:flutter/material.dart';
 
 class BtcController {
 
-
-  List<String> _fiatCurrencies = currencies.fiatCurrencies;
-  List<String> _cryptoCurrencies = currencies.cryptoCurrencies;
-
-  List<Crypto> _cryptoList = [];
+  List<CryptoRatio> _cryptoRatioList = [];
+  List<Widget> _currencyCards = [];
   String _currentFiat = 'USD';
 
   get getCurrentFiat => _currentFiat;
-  get getFiatCurrencies => _fiatCurrencies;
+  get getFiatCurrencies => currencies.fiatCurrencyNames;
   set setCurrentFiat(String newFiat) => _currentFiat = newFiat;
 
   Future<void> getCryptoRate() async {
-    _cryptoList = await WebServices.getCryptoRate(_cryptoCurrencies, _fiatCurrencies);
-    _cryptoList.forEach((element) => print(element.toString())); //print verification
+    _cryptoRatioList = await WebServices.getCryptoRate(currencies.cryptoCurrencyNames, currencies.fiatCurrencyNames);
+    _cryptoRatioList.forEach((crypto) => print(crypto.toString())); //print verification
+  }
+
+  ///for each crypto, it save current rate when for this crypto, fiat name & current fiat are same & add a CryptoCardWidget to widget List !
+  ///
+  ///At the end, it returns the list widget with one CryptoRatioWidget by crypto
+  List<Widget> getCurrencyCards () {
+    String _currentRate = '?';
+    _currencyCards.clear();
+    currencies.cryptoCurrencyNames.forEach((cryptoName) {
+      _cryptoRatioList.forEach((cryptoRatio) {
+        if(cryptoRatio.cryptoName == cryptoName && cryptoRatio.fiatName == _currentFiat) {
+          _currentRate = cryptoRatio.rate.toString();
+        }
+      });
+      _currencyCards.add(CryptoRatioCard(cryptoName: cryptoName, rate: _currentRate, fiatName: _currentFiat));
+    });
+    return _currencyCards;
   }
 }
