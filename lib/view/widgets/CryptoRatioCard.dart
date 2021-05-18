a91ee7cf405593d6fe6620f765040d21b0b743d9
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-18 18:25:51 +0200

    Create a Card Widget to display a crypto & it's cost in a specific fiat currency

---
+++ lib/view/widgets/CryptoRatioCard.dart
@@ -0,0 +1,37 @@
+import 'package:bitcoin_ticker/utilities/AppConst.dart';
+import 'package:flutter/material.dart';
+
+class CryptoRatioCard extends StatelessWidget{
+
+  final String cryptoName;
+  final String rate;
+  final String fiatName;
+
+  CryptoRatioCard({
+    @required this.cryptoName,
+    @required this.rate,
+    @required this.fiatName
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoName = $rate $fiatName',
+            textAlign: TextAlign.center,
+            style: AppConst.mainTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
