e898ced44c33320f9a0bd47644592a2ae5ca6d42
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-25 19:45:59 +0200

    add a progress indicator when data are loaded + change color for fiat currency selector

--- lib/view/screens/price_screen.dart
+++ lib/view/screens/price_screen.dart
@@ -2,6 +2,7 @@ import 'package:bitcoin_ticker/controller/BtcController.dart';
 import 'package:bitcoin_ticker/view/widgets/AppItemSelector.dart';
 import 'package:bitcoin_ticker/utilities/AppConst.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -11,62 +12,67 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
 
   CoinTickerBrain _btcController = CoinTickerBrain();
+  bool _isLoading = false;
 
   @override
   void initState()  {
     super.initState();
-    initScreenValues();
+    getScreenValues();
   }
 
-  void initScreenValues() async {
+  void getScreenValues() async {
+    setState(() => _isLoading = true);
     await _btcController.getCryptoRate();
-    setState(() => _btcController.updateCurrencyCards());
+    setState(() {
+      _btcController.updateCurrencyCards();
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Text('Coin Ticker'),
         actions: [
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () async {
-              await _btcController.getCryptoRate();
-              setState(() => _btcController.updateCurrencyCards());
-            },
+            onPressed: () async => getScreenValues()
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.75,
-            child: SingleChildScrollView(
-              child: Column(
-                children: _btcController.updateCurrencyCards()
+      body: ModalProgressHUD(
+        inAsyncCall: _isLoading,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: _btcController.updateCurrencyCards()
+                ),
               ),
             ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.1,
-            color: Colors.lightBlue,
-            alignment: Alignment.center,
-            child: AppItemSelector(
-              androidDropdownMenuItems: _btcController.getAndroidCurrencyItems(),
-              androidDefaultValue: _btcController.getCurrentFiat,
-              onAndroidChanged: (newFiatCurrency) {
-                setState(() => _btcController.setCurrentFiat = newFiatCurrency);
-              },
-              iosPickerItems: _btcController.getIosCurrencyItems(),
-              onIosSelectedItemChanged: (index) {
-                setState(() => _btcController.setCurrentFiat = _btcController.getFiatCurrencies[index]);
-              },
+            Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              color: Colors.deepOrange,
+              alignment: Alignment.center,
+              child: AppItemSelector(
+                androidDropdownMenuItems: _btcController.getAndroidCurrencyItems(),
+                androidDefaultValue: _btcController.getCurrentFiat,
+                onAndroidChanged: (newFiatCurrency) {
+                  setState(() => _btcController.setCurrentFiat = newFiatCurrency);
+                },
+                iosPickerItems: _btcController.getIosCurrencyItems(),
+                onIosSelectedItemChanged: (index) {
+                  setState(() => _btcController.setCurrentFiat = _btcController.getFiatCurrencies[index]);
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
