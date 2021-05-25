cb6f68cd4d24ca6e25b066e2a5201bf71049de04
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-25 19:11:10 +0200

    fix orientation to portraitUp + remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'view/screens/price_screen.dart';
 
 void main() => runApp(MyApp());
@@ -6,7 +7,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
           primaryColor: Colors.lightBlue,
           scaffoldBackgroundColor: Colors.white),
