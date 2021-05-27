c9eeb944d0081e5609943d40b5f83447b84cf196
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-27 18:21:07 +0200

    refactor webService method (URI instance)

--- lib/services/ApiCoinConst.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bitcoin_ticker/services/privateKey.dart';
-
-class ApiCoinConst {
-  static const String authority = 'min-api.cryptocompare.com';
-  static const String unencodedPath = '/data/pricemulti';
-  static const String apiKey = privateKey; //value come from a file not followed by git
-}
--- lib/services/WebServices.dart
+++ lib/services/WebServices.dart
@@ -1,8 +1,11 @@
 import 'package:bitcoin_ticker/model/CryptoRatio.dart';
-import 'package:bitcoin_ticker/services/ApiCoinConst.dart';
+import 'package:bitcoin_ticker/services/privateKey.dart';
 import 'HttpRequest.dart';
 
 class WebServices {
+
+  static const String baseURL = 'https://min-api.cryptocompare.com/data/pricemulti';
+  static const String apiKey = privateKey;
 
   static Future<List<CryptoRatio>> getCryptoRate(List<String> cryptoCurrencies, List<String> fiatCurrencies) async {
 
@@ -11,16 +14,8 @@ class WebServices {
     cryptoCurrencies.forEach((crypto) => cryptoCurrenciesAsString += '$crypto,');
     fiatCurrencies.forEach((fiat) => fiatCurrenciesAsString += '$fiat,');
 
-    Uri url = Uri.https(
-      ApiCoinConst.authority,
-      ApiCoinConst.unencodedPath,
-      {
-        'fsyms' : cryptoCurrenciesAsString ,
-        'tsyms' : fiatCurrenciesAsString,
-        'api_key' : ApiCoinConst.apiKey
-      }
-    );
-
+    // url example : https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR&api_key={privateKey}
+    Uri url = Uri.parse('$baseURL?fsyms=$cryptoCurrenciesAsString&tsyms=$fiatCurrenciesAsString&api_key=$apiKey');
     dynamic data = await HttpRequest.httpRequestGet(url);
 
     List<CryptoRatio> cryptoList = [];
