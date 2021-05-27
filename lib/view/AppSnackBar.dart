6837798702d4fe449aff85ef20a9df2a13007982
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-27 18:21:36 +0200

    add a snackBar object

---
+++ lib/view/AppSnackBar.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class AppSnackBar {
+
+  static SnackBar getErrorSnackBar(String errorMessage) {
+    return SnackBar(
+      backgroundColor: Colors.red,
+      content: Text(
+        errorMessage,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
