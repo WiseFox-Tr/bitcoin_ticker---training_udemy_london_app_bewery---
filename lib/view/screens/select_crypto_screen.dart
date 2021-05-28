582368eaf2c458bd1dc638bf9d99390c83f4391d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-28 22:26:50 +0200

    add a method to generate all cryptoNameCards + add callback when user finished to select his cryptos

--- lib/view/screens/select_crypto_screen.dart
+++ lib/view/screens/select_crypto_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bitcoin_ticker/controller/CoinTickerBrain.dart';
 import 'package:bitcoin_ticker/utilities/AppConst.dart';
+import 'package:bitcoin_ticker/view/widgets/CryptoNameCard.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:flutter/material.dart';
 
 class SelectCryptoScreen extends StatefulWidget {
@@ -7,15 +10,48 @@ class SelectCryptoScreen extends StatefulWidget {
 }
 
 class _SelectCryptoScreenState extends State<SelectCryptoScreen> {
+
+  CoinTickerBrain _coinTickerBrain = CoinTickerBrain();
+
+  //get a CryptoNameCard List composed with all cryptoCurrency objects
+  List<Widget> updateCryptoNameCards() {
+    List<Widget> _cryptoNameCards = [];
+    _coinTickerBrain.getCryptoCurrenciesList.forEach((crypto) {
+      _cryptoNameCards.add(
+        CryptoNameCard(
+          label: crypto.name,
+          isFollowed: crypto.isFollowed,
+          onTap: () {
+            setState(() {
+              _coinTickerBrain.setCryptoCurrencyFollowedStatus(_coinTickerBrain.getSpecificCryptoCurrency(crypto.name), !crypto.isFollowed);
+            });
+          }
+        )
+      );
+    });
+    return _cryptoNameCards;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppConst.strTitle),
       ),
-      body: Center(),
+      body: ModalProgressHUD(
+        inAsyncCall: _coinTickerBrain.getIsLoading,
+        child: GridView.count(
+          crossAxisCount: 3,
+          children: updateCryptoNameCards(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pop(context),
+        onPressed: () async {
+          setState(() => _coinTickerBrain.setIsLoading = true);
+          await _coinTickerBrain.getCryptoPrices(context);
+          setState(() => _coinTickerBrain.setIsLoading = false);
+          Navigator.pop(context);
+        },
         child: Icon(Icons.check),
       ),
     );
