64a5875e667286799ff5eb6d3303e30003c45cbf
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-28 22:39:09 +0200

    add custom getter & setter for specefic crypto modification + add a method that returns a list of followed crypto + some name changes + delete push method

--- lib/controller/CoinTickerBrain.dart
+++ lib/controller/CoinTickerBrain.dart
@@ -1,10 +1,11 @@
+import 'package:bitcoin_ticker/model/CryptoCurrency.dart';
 import 'package:bitcoin_ticker/model/CryptoRatio.dart';
 import 'package:bitcoin_ticker/services/WebServices.dart';
 import 'package:bitcoin_ticker/utilities/AppConst.dart';
 import 'package:bitcoin_ticker/utilities/coin_data.dart' as currencies;
 import 'package:bitcoin_ticker/utilities/ErrorManager.dart';
 import 'package:bitcoin_ticker/view/AppSnackBar.dart';
-import 'package:bitcoin_ticker/view/screens/select_crypto_screen.dart';
+import 'package:bitcoin_ticker/view/widgets/CryptoNameCard.dart';
 import 'package:bitcoin_ticker/view/widgets/CryptoRatioCard.dart';
 import 'package:flutter/material.dart';
 
@@ -20,10 +21,23 @@ class CoinTickerBrain {
   set setCurrentFiat(String newFiat) => _currentFiat = newFiat;
   set setIsLoading(bool newBool) => _isLoading = newBool;
 
+  CryptoCurrency getSpecificCryptoCurrency(String cryptoName) => currencies.cryptoCurrenciesList.firstWhere((crypto) => crypto.name == cryptoName);
+  void setCryptoCurrencyFollowedStatus(CryptoCurrency cryptoToUpdate, bool newStatus) => cryptoToUpdate.isFollowed = newStatus;
+
+  ///returns a list containing only followed crypto
+  List<CryptoCurrency> getFollowedCryptoList() {
+    List<CryptoCurrency> followedCryptoList = [];
+    currencies.cryptoCurrenciesList.forEach((crypto) {
+      if(crypto.isFollowed)
+        followedCryptoList.add(crypto);
+    });
+    return followedCryptoList;
+  }
+
   ///retrieve crypto & fiat currencies price data or display a custom error message
   Future<void> getCryptoPrices(BuildContext context) async {
     try {
-      _cryptoRatioList = await WebServices.getCryptoRate(currencies.cryptoCurrenciesList, currencies.fiatCurrencyNames);
+      _cryptoRatioList = await WebServices.getCryptoRate(getFollowedCryptoList(), currencies.fiatCurrencyNames);
       _cryptoRatioList.forEach((crypto) => print(crypto.toString())); //print verification
     } catch(e) {
       print('exception : $e');
@@ -31,24 +45,24 @@ class CoinTickerBrain {
     }
   }
 
-  ///for each crypto, it saves current rate when for this crypto, fiat name & current fiat are same & add a CryptoCardWidget to widget List !
+  ///for each crypto followed, it saves current rate when for this crypto, fiat name & current fiat are same & add a CryptoCardWidget to widget List !
   ///
-  ///At the end, it returns the list widget with one CryptoRatioWidget by crypto
-  List<Widget> updateCurrencyCards () {
-    List<Widget> _currencyCards = [];
+  ///At the end, it returns the list widget with one CryptoRatioWidget by crypto followed
+  List<Widget> updateCryptoRatioCards() {
+    List<Widget> _cryptoRatioCards = [];
     String _currentRate = '?';
-    currencies.cryptoCurrenciesList.forEach((crypto) {
+    getFollowedCryptoList().forEach((crypto) {
       _cryptoRatioList.forEach((cryptoRatio) {
         if(cryptoRatio.cryptoName == crypto.name && cryptoRatio.fiatName == _currentFiat) {
           _currentRate = cryptoRatio.price.toString();
         }
       });
-      _currencyCards.add(CryptoRatioCard(cryptoName: crypto.name, rate: _currentRate, fiatName: _currentFiat));
+      _cryptoRatioCards.add(CryptoRatioCard(cryptoName: crypto.name, rate: _currentRate, fiatName: _currentFiat));
     });
-    return _currencyCards;
+    return _cryptoRatioCards;
   }
 
-  ///-------- APP SELECTOR methods --------------
+  //-------- APP SELECTOR methods --------------
   List<Text> getIosCurrencyItems() {
     List<Text> fiatCurrencies = [];
     currencies.fiatCurrencyNames.forEach((fiatCurrency) {
@@ -61,13 +75,5 @@ class CoinTickerBrain {
     return currencies.fiatCurrencyNames.map<DropdownMenuItem<String>>((String value) {
       return DropdownMenuItem(value: value, child: Text(value));
     }).toList();
-  }
-
-  ///-------- ROUTE method --------------
-  void goToSelectCryptoScreen(BuildContext context) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => SelectCryptoScreen()),
-    );
   }
 }
