f84fee1088d3e9934bcbb0fdaafe0f88c16ae1e4
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-28 19:59:09 +0200

    add appBar & FAB

--- lib/view/screens/select_crypto_screen.dart
+++ lib/view/screens/select_crypto_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bitcoin_ticker/utilities/AppConst.dart';
 import 'package:flutter/material.dart';
 
 class SelectCryptoScreen extends StatefulWidget {
@@ -8,6 +9,15 @@ class SelectCryptoScreen extends StatefulWidget {
 class _SelectCryptoScreenState extends State<SelectCryptoScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppConst.strTitle),
+      ),
+      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.pop(context),
+        child: Icon(Icons.check),
+      ),
+    );
   }
 }
