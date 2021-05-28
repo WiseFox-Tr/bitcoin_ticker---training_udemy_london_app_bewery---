953c1ea60d8e435db501e9fbe249756ece5073b5
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-28 22:25:47 +0200

    add new card for crypto selection

---
+++ lib/view/widgets/CryptoNameCard.dart
@@ -0,0 +1,22 @@
+import 'package:bitcoin_ticker/utilities/AppConst.dart';
+import 'package:flutter/material.dart';
+
+class CryptoNameCard extends StatelessWidget {
+
+  final String label;
+  final bool isFollowed;
+  final Function onTap;
+
+  CryptoNameCard({@required this.label, @required this.isFollowed, @required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        color: isFollowed ? Colors.orange : Colors.orange[100],
+        child: Text(label, style: AppConst.mainTextStyle,),
+      ),
+    );
+  }
+}
