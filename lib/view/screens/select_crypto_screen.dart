c12e2ba6673e765c8b47a5688aea3a72402052c6
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-28 18:31:42 +0200

    create a new screen to select crypto user

---
+++ lib/view/screens/select_crypto_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SelectCryptoScreen extends StatefulWidget {
+  @override
+  _SelectCryptoScreenState createState() => _SelectCryptoScreenState();
+}
+
+class _SelectCryptoScreenState extends State<SelectCryptoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
