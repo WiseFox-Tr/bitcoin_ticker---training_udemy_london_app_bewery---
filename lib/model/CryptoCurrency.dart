e0a2439876f65234a1b353a582466d60bebb63e3
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-28 20:23:32 +0200

    create a CryptoCurrency class

---
+++ lib/model/CryptoCurrency.dart
@@ -0,0 +1,7 @@
+//cryptoCurrency class to store if crypto is followed by user --> this is that to store into SharedPreference
+class CryptoCurrency {
+  String name;
+  bool isFollowed;
+
+  CryptoCurrency(this.name, this.isFollowed);
+}
