4244036bcd3df4977fffb05a76aeb8b8461b51b7
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-04 12:16:52 +0200

    simplify errorManager

--- lib/utilities/ErrorManager.dart
+++ lib/utilities/ErrorManager.dart
@@ -1,10 +1,7 @@
-class ErrorManager {
-  static const String baseErrorMessage = "Oups, nous n'avons pas réussi à récupérer les données";
+import 'package:bitcoin_ticker/dataResources/AppConst.dart';
 
+class ErrorManager {
   static String getErrorForUser(String exceptionMessage) {
-    if(exceptionMessage.contains("Failed host lookup"))
-      return "$baseErrorMessage, pensez à vérifier votre connexion internet ou réessayez plus tard.";
-    else
-      return '$baseErrorMessage, réessayez plus tard !';
+    return exceptionMessage.contains("Failed host lookup") ? AppConst.strCheckInternetConnectionError : AppConst.strDefaultError;
   }
 }
