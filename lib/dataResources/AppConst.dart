7aa2703c9e00abe907500109e62ea70cd6ffe759
Maxime Traverse <maxime.Traverse@orange.fr>
2021-06-04 12:16:41 +0200

    add error message as AppConst strings

--- lib/dataResources/AppConst.dart
+++ lib/dataResources/AppConst.dart
@@ -6,4 +6,7 @@ class AppConst {
   static const TextStyle mainTextStyle = TextStyle(color: Colors.black87 ,fontSize: 20.0, fontWeight: FontWeight.w800);
 
   static const String strTitle = 'Coin Ticker';
+  static const String strBaseErrorMessage = "Oups, nous n'avons pas réussi à récupérer les données";
+  static const String strCheckInternetConnectionError = "$strBaseErrorMessage, pensez à vérifier votre connexion internet ou réessayez plus tard.";
+  static const String strDefaultError = '$strBaseErrorMessage, réessayez plus tard !';
 }
